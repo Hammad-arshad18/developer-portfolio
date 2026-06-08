58c443860132e3ee0013f790907f62771ecf5442
Hammad Arshad <hammad.arshad672@gmail.com>
2026-06-08 17:24:51 +0400

    chore: rename project and configure GitHub Pages deployment settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
     return {
-        base: '/developer-portfolio/',
+        base: '/developer-portfolio',
         plugins: [react(), tailwindcss()],
         resolve: {
             alias: {
